b17613ee5463baa2baa0bdbe698296a9a0817b93
suna8788 <61766463+suna8788@users.noreply.github.com>
2020-03-04 11:22:35 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,930 @@
+ var data = [{
+   "hanja": "口",
+   "jei_set": "가01",
+   "mean": "입 구"
+ }, {
+   "hanja": "目",
+   "jei_set": "가01",
+   "mean": "눈 목"
+ }, {
+   "hanja": "耳",
+   "jei_set": "가01",
+   "mean": "귀 이"
+ }, {
+   "hanja": "手",
+   "jei_set": "가01",
+   "mean": "손 수"
+ }, {
+   "hanja": "力",
+   "jei_set": "가01",
+   "mean": "힘 력"
+ }, {
+   "hanja": "日",
+   "jei_set": "가02",
+   "mean": "날 일"
+ }, {
+   "hanja": "月",
+   "jei_set": "가02",
+   "mean": "달 월"
+ }, {
+   "hanja": "火",
+   "jei_set": "가02",
+   "mean": "불 화"
+ }, {
+   "hanja": "水",
+   "jei_set": "가02",
+   "mean": "물 수"
+ }, {
+   "hanja": "木",
+   "jei_set": "가02",
+   "mean": "나무 목"
+ }, {
+   "hanja": "山",
+   "jei_set": "가03",
+   "mean": "산 산"
+ }, {
+   "hanja": "雨",
+   "jei_set": "가03",
+   "mean": "비 우"
+ }, {
+   "hanja": "田",
+   "jei_set": "가03",
+   "mean": "밭 전"
+ }, {
+   "hanja": "石",
+   "jei_set": "가03",
+   "mean": "돌 석"
+ }, {
+   "hanja": "中",
+   "jei_set": "가03",
+   "mean": "가운데 중"
+ }, {
+   "hanja": "犬",
+   "jei_set": "가04",
+   "mean": "개 견"
+ }, {
+   "hanja": "魚",
+   "jei_set": "가04",
+   "mean": "물고기 어"
+ }, {
+   "hanja": "馬",
+   "jei_set": "가04",
+   "mean": "말 마"
+ }, {
+   "hanja": "豕",
+   "jei_set": "가04",
+   "mean": "돼지 시"
+ }, {
+   "hanja": "鳥",
+   "jei_set": "가04",
+   "mean": "새 조"
+ }, {
+   "hanja": "人",
+   "jei_set": "가05",
+   "mean": "사람 인"
+ }, {
+   "hanja": "大",
+   "jei_set": "가05",
+   "mean": "큰 대"
+ }, {
+   "hanja": "女",
+   "jei_set": "가05",
+   "mean": "여자 녀"
+ }, {
+   "hanja": "子",
+   "jei_set": "가05",
+   "mean": "아들 자"
+ }, {
+   "hanja": "立",
+   "jei_set": "가05",
+   "mean": "설 립"
+ }, {
+   "hanja": "羊",
+   "jei_set": "가06",
+   "mean": "양 양"
+ }, {
+   "hanja": "牛",
+   "jei_set": "가06",
+   "mean": "소 우"
+ }, {
+   "hanja": "象",
+   "jei_set": "가06",
+   "mean": "코끼리 상"
+ }, {
+   "hanja": "鹿",
+   "jei_set": "가06",
+   "mean": "사슴 록"
+ }, {
+   "hanja": "虎",
+   "jei_set": "가06",
+   "mean": "호랑이 호"
+ }, {
+   "hanja": "冊",
+   "jei_set": "가07",
+   "mean": "책 책"
+ }, {
+   "hanja": "車",
+   "jei_set": "가07",
+   "mean": "수레 거/차"
+ }, {
+   "hanja": "門",
+   "jei_set": "가07",
+   "mean": "문 문"
+ }, {
+   "hanja": "刀",
+   "jei_set": "가07",
+   "mean": "칼 도"
+ }, {
+   "hanja": "絲",
+   "jei_set": "가07",
+   "mean": "실 사"
+ }, {
+   "hanja": "休",
+   "jei_set": "가08",
+   "mean": "쉴 휴"
+ }, {
+   "hanja": "畓",
+   "jei_set": "가08",
+   "mean": "논 답"
+ }, {
+   "hanja": "男",
+   "jei_set": "가08",
+   "mean": "사내 남"
+ }, {
+   "hanja": "伏",
+   "jei_set": "가08",
+   "mean": "엎드릴 복"
+ }, {
+   "hanja": "明",
+   "jei_set": "가08",
+   "mean": "밝을 명"
+ }, {
+   "hanja": "林",
+   "jei_set": "가09",
+   "mean": "수풀 림"
+ }, {
+   "hanja": "鳴",
+   "jei_set": "가09",
+   "mean": "울 명"
+ }, {
+   "hanja": "好",
+   "jei_set": "가09",
+   "mean": "좋을 호"
+ }, {
+   "hanja": "果",
+   "jei_set": "가09",
+   "mean": "열매 과"
+ }, {
+   "hanja": "天",
+   "jei_set": "가09",
+   "mean": "하늘 천"
+ }, {
+   "hanja": "一",
+   "jei_set": "가10",
+   "mean": "하나 일"
+ }, {
+   "hanja": "二",
+   "jei_set": "가10",
+   "mean": "두 이"
+ }, {
+   "hanja": "三",
+   "jei_set": "가10",
+   "mean": "셋 삼"
+ }, {
+   "hanja": "上",
+   "jei_set": "가10",
+   "mean": "위 상"
+ }, {
+   "hanja": "下",
+   "jei_set": "가10",
+   "mean": "아래 하"
+ }, {
+   "hanja": "矢",
+   "jei_set": "나01",
+   "mean": "화살 시"
+ }, {
+   "hanja": "弓",
+   "jei_set": "나01",
+   "mean": "활 궁"
+ }, {
+   "hanja": "刃",
+   "jei_set": "나01",
+   "mean": "칼날 인"
+ }, {
+   "hanja": "工",
+   "jei_set": "나01",
+   "mean": "장인 공"
+ }, {
+   "hanja": "至",
+   "jei_set": "나01",
+   "mean": "이를 지"
+ }, {
+   "hanja": "央",
+   "jei_set": "나02",
+   "mean": "가운데 앙"
+ }, {
+   "hanja": "老",
+   "jei_set": "나02",
+   "mean": "늙을 로"
+ }, {
+   "hanja": "囚",
+   "jei_set": "나02",
+   "mean": "가둘 수"
+ }, {
+   "hanja": "己",
+   "jei_set": "나02",
+   "mean": "몸 기"
+ }, {
+   "hanja": "孝",
+   "jei_set": "나02",
+   "mean": "효도 효"
+ }, {
+   "hanja": "土",
+   "jei_set": "나03",
+   "mean": "흙 토"
+ }, {
+   "hanja": "才",
+   "jei_set": "나03",
+   "mean": "재주 재"
+ }, {
+   "hanja": "生",
+   "jei_set": "나03",
+   "mean": "날 생"
+ }, {
+   "hanja": "靑",
+   "jei_set": "나03",
+   "mean": "푸를 청"
+ }, {
+   "hanja": "苗",
+   "jei_set": "나03",
+   "mean": "싹 묘"
+ }, {
+   "hanja": "又",
+   "jei_set": "나04",
+   "mean": "또 우"
+ }, {
+   "hanja": "左",
+   "jei_set": "나04",
+   "mean": "왼쪽 좌"
+ }, {
+   "hanja": "右",
+   "jei_set": "나04",
+   "mean": "오른쪽 우"
+ }, {
+   "hanja": "友",
+   "jei_set": "나04",
+   "mean": "벗 우"
+ }, {
+   "hanja": "取",
+   "jei_set": "나04",
+   "mean": "취할 취"
+ }, {
+   "hanja": "羽",
+   "jei_set": "나05",
+   "mean": "깃 우"
+ }, {
+   "hanja": "烏",
+   "jei_set": "나05",
+   "mean": "까마귀 오"
+ }, {
+   "hanja": "飛",
+   "jei_set": "나05",
+   "mean": "날 비"
+ }, {
+   "hanja": "隹",
+   "jei_set": "나05",
+   "mean": "새 추"
+ }, {
+   "hanja": "燕",
+   "jei_set": "나05",
+   "mean": "제비 연"
+ }, {
+   "hanja": "本",
+   "jei_set": "나06",
+   "mean": "근본 본"
+ }, {
+   "hanja": "末",
+   "jei_set": "나06",
+   "mean": "끝 말"
+ }, {
+   "hanja": "森",
+   "jei_set": "나06",
+   "mean": "빽빽할 삼"
+ }, {
+   "hanja": "集",
+   "jei_set": "나06",
+   "mean": "모일 집"
+ }, {
+   "hanja": "片",
+   "jei_set": "나06",
+   "mean": "조각 편"
+ }, {
+   "hanja": "言",
+   "jei_set": "나07",
+   "mean": "말씀 언"
+ }, {
+   "hanja": "舌",
+   "jei_set": "나07",
+   "mean": "혀 설"
+ }, {
+   "hanja": "音",
+   "jei_set": "나07",
+   "mean": "소리 음"
+ }, {
+   "hanja": "齒",
+   "jei_set": "나07",
+   "mean": "이 치"
+ }, {
+   "hanja": "甘",
+   "jei_set": "나07",
+   "mean": "달 감"
+ }, {
+   "hanja": "入",
+   "jei_set": "나08",
+   "mean": "들 입"
+ }, {
+   "hanja": "內",
+   "jei_set": "나08",
+   "mean": "안 내"
+ }, {
+   "hanja": "戶",
+   "jei_set": "나08",
+   "mean": "집 호"
+ }, {
+   "hanja": "穴",
+   "jei_set": "나08",
+   "mean": "굴 혈"
+ }, {
+   "hanja": "高",
+   "jei_set": "나08",
+   "mean": "높을 고"
+ }, {
+   "hanja": "角",
+   "jei_set": "나09",
+   "mean": "뿔 각"
+ }, {
+   "hanja": "毛",
+   "jei_set": "나09",
+   "mean": "털 모"
+ }, {
+   "hanja": "肉",
+   "jei_set": "나09",
+   "mean": "고기 육"
+ }, {
+   "hanja": "心",
+   "jei_set": "나09",
+   "mean": "마음 심"
+ }, {
+   "hanja": "骨",
+   "jei_set": "나09",
+   "mean": "뼈 골"
+ }, {
+   "hanja": "自",
+   "jei_set": "나10",
+   "mean": "스스로 자"
+ }, {
+   "hanja": "首",
+   "jei_set": "나10",
+   "mean": "머리 수"
+ }, {
+   "hanja": "面",
+   "jei_set": "나10",
+   "mean": "얼굴 면"
+ }, {
+   "hanja": "頁",
+   "jei_set": "나10",
+   "mean": "머리 혈"
+ }, {
+   "hanja": "思",
+   "jei_set": "나10",
+   "mean": "생각할 사"
+ }, {
+   "hanja": "川",
+   "jei_set": "나11",
+   "mean": "내 천"
+ }, {
+   "hanja": "州",
+   "jei_set": "나11",
+   "mean": "고을 주"
+ }, {
+   "hanja": "泉",
+   "jei_set": "나11",
+   "mean": "샘 천"
+ }, {
+   "hanja": "原",
+   "jei_set": "나11",
+   "mean": "근원 원"
+ }, {
+   "hanja": "永",
+   "jei_set": "나11",
+   "mean": "길 영"
+ }, {
+   "hanja": "出",
+   "jei_set": "나12",
+   "mean": "날 출"
+ }, {
+   "hanja": "足",
+   "jei_set": "나12",
+   "mean": "발 족"
+ }, {
+   "hanja": "走",
+   "jei_set": "나12",
+   "mean": "달릴 주"
+ }, {
+   "hanja": "止",
+   "jei_set": "나12",
+   "mean": "그칠 지"
+ }, {
+   "hanja": "步",
+   "jei_set": "나12",
+   "mean": "걸을 보"
+ }, {
+   "hanja": "舟",
+   "jei_set": "나13",
+   "mean": "배 주"
+ }, {
+   "hanja": "竹",
+   "jei_set": "나13",
+   "mean": "대 죽"
+ }, {
+   "hanja": "蟲",
+   "jei_set": "나13",
+   "mean": "벌레 충"
+ }, {
+   "hanja": "貝",
+   "jei_set": "나13",
+   "mean": "조개 패"
+ }, {
+   "hanja": "求",
+   "jei_set": "나13",
+   "mean": "구할 구"
+ }, {
+   "hanja": "書",
+   "jei_set": "나14",
+   "mean": "글 서"
+ }, {
+   "hanja": "雪",
+   "jei_set": "나14",
+   "mean": "눈 설"
+ }, {
+   "hanja": "筆",
+   "jei_set": "나14",
+   "mean": "붓 필"
+ }, {
+   "hanja": "畵",
+   "jei_set": "나14",
+   "mean": "그림 화"
+ }, {
+   "hanja": "事",
+   "jei_set": "나14",
+   "mean": "일 사"
+ }, {
+   "hanja": "母",
+   "jei_set": "나15",
+   "mean": "어미 모"
+ }, {
+   "hanja": "安",
+   "jei_set": "나15",
+   "mean": "편안할 안"
+ }, {
+   "hanja": "美",
+   "jei_set": "나15",
+   "mean": "아름다울 미"
+ }, {
+   "hanja": "化",
+   "jei_set": "나15",
+   "mean": "될 화"
+ }, {
+   "hanja": "育",
+   "jei_set": "나15",
+   "mean": "기를 육"
+ }, {
+   "hanja": "皿",
+   "jei_set": "나16",
+   "mean": "그릇 명"
+ }, {
+   "hanja": "酉",
+   "jei_set": "나16",
+   "mean": "닭 유"
+ }, {
+   "hanja": "豆",
+   "jei_set": "나16",
+   "mean": "콩 두"
+ }, {
+   "hanja": "血",
+   "jei_set": "나16",
+   "mean": "피 혈"
+ }, {
+   "hanja": "益",
+   "jei_set": "나16",
+   "mean": "더할 익"
+ }, {
+   "hanja": "問",
+   "jei_set": "나17",
+   "mean": "물을 문"
+ }, {
+   "hanja": "聞",
+   "jei_set": "나17",
+   "mean": "들을 문"
+ }, {
+   "hanja": "間",
+   "jei_set": "나17",
+   "mean": "사이 간"
+ }, {
+   "hanja": "開",
+   "jei_set": "나17",
+   "mean": "열 개"
+ }, {
+   "hanja": "閉",
+   "jei_set": "나17",
+   "mean": "닫을 폐"
+ }, {
+   "hanja": "旦",
+   "jei_set": "나18",
+   "mean": "아침 단"
+ }, {
+   "hanja": "昌",
+   "jei_set": "나18",
+   "mean": "창성할 창"
+ }, {
+   "hanja": "莫",
+   "jei_set": "나18",
+   "mean": "없을 막"
+ }, {
+   "hanja": "白",
+   "jei_set": "나18",
+   "mean": "흰 백"
+ }, {
+   "hanja": "早",
+   "jei_set": "나18",
+   "mean": "이를 조"
+ }, {
+   "hanja": "云",
+   "jei_set": "나19",
+   "mean": "말할 운"
+ }, {
+   "hanja": "雲",
+   "jei_set": "나19",
+   "mean": "구름 운"
+ }, {
+   "hanja": "申",
+   "jei_set": "나19",
+   "mean": "펼 신"
+ }, {
+   "hanja": "電",
+   "jei_set": "나19",
+   "mean": "번개 전"
+ }, {
+   "hanja": "米",
+   "jei_set": "나19",
+   "mean": "쌀 미"
+ }, {
+   "hanja": "小",
+   "jei_set": "나20",
+   "mean": "작을 소"
+ }, {
+   "hanja": "氣",
+   "jei_set": "나20",
+   "mean": "기운 기"
+ }, {
+   "hanja": "金",
+   "jei_set": "나20",
+   "mean": "쇠 금"
+ }, {
+   "hanja": "太",
+   "jei_set": "나20",
+   "mean": "클 태"
+ }, {
+   "hanja": "少",
+   "jei_set": "나20",
+   "mean": "적을 소"
+ }, {
+   "hanja": "果刀",
+   "jei_set": "다30",
+   "mean": "과도"
+ }, {
+   "hanja": "男女",
+   "jei_set": "다30",
+   "mean": "남녀"
+ }, {
+   "hanja": "男子",
+   "jei_set": "다30",
+   "mean": "남자"
+ }, {
+   "hanja": "大門",
+   "jei_set": "다30",
+   "mean": "대문"
+ }, {
+   "hanja": "大魚",
+   "jei_set": "다30",
+   "mean": "대어"
+ }, {
+   "hanja": "大人",
+   "jei_set": "다30",
+   "mean": "대인"
+ }, {
+   "hanja": "馬耳",
+   "jei_set": "다30",
+   "mean": "마이"
+ }, {
+   "hanja": "馬車",
+   "jei_set": "다30",
+   "mean": "마차"
+ }, {
+   "hanja": "明月",
+   "jei_set": "다30",
+   "mean": "명월"
+ }, {
+   "hanja": "木馬",
+   "jei_set": "다30",
+   "mean": "목마"
+ }, {
+   "hanja": "木石",
+   "jei_set": "다30",
+   "mean": "목석"
+ }, {
+   "hanja": "山林",
+   "jei_set": "다30",
+   "mean": "산림"
+ }, {
+   "hanja": "山上",
+   "jei_set": "다30",
+   "mean": "산중"
+ }, {
+   "hanja": "山羊",
+   "jei_set": "다30",
+   "mean": "산양"
+ }, {
+   "hanja": "山中",
+   "jei_set": "다30",
+   "mean": "산중"
+ }, {
+   "hanja": "水力",
+   "jei_set": "다30",
+   "mean": "수력"
+ }, {
+   "hanja": "水上",
+   "jei_set": "다30",
+   "mean": "수상"
+ }, {
+   "hanja": "水中",
+   "jei_set": "다30",
+   "mean": "수중"
+ }, {
+   "hanja": "女人",
+   "jei_set": "다30",
+   "mean": "여인"
+ }, {
+   "hanja": "牛耳",
+   "jei_set": "다30",
+   "mean": "우이"
+ }, {
+   "hanja": "耳目",
+   "jei_set": "다30",
+   "mean": "이목"
+ }, {
+   "hanja": "人力",
+   "jei_set": "다30",
+   "mean": "인력"
+ }, {
+   "hanja": "日月",
+   "jei_set": "다30",
+   "mean": "일월"
+ }, {
+   "hanja": "子女",
+   "jei_set": "다30",
+   "mean": "자녀"
+ }, {
+   "hanja": "田畓",
+   "jei_set": "다30",
+   "mean": "전답"
+ }, {
+   "hanja": "天上",
+   "jei_set": "다30",
+   "mean": "천상"
+ }, {
+   "hanja": "天下",
+   "jei_set": "다30",
+   "mean": "천하"
+ }, {
+   "hanja": "下車",
+   "jei_set": "다30",
+   "mean": "하차"
+ }, {
+   "hanja": "好人",
+   "jei_set": "다30",
+   "mean": "호인"
+ }, {
+   "hanja": "火力",
+   "jei_set": "다30",
+   "mean": "화력"
+ }, {
+   "hanja": "火山",
+   "jei_set": "다30",
+   "mean": "화산"
+ }, {
+   "hanja": "休日",
+   "jei_set": "다30",
+   "mean": "휴일"
+}, {
+  "hanja": "校",
+  "jei_set": "급08",
+  "mean": "학교/교정할 교"
+}, {
+  "hanja": "敎",
+  "jei_set": "급08",
+  "mean": "가르칠 교"
+},  {
+  "hanja": "九",
+  "jei_set": "급08",
+  "mean": "아홉 구"
+},  {
+  "hanja": "金",
+  "jei_set": "급08",
+  "mean": "쇠 금"
+},  {
+  "hanja": "己",
+  "jei_set": "급08",
+  "mean": "몸 기"
+},  {
+  "hanja": "男",
+  "jei_set": "급08",
+  "mean": "사내 남"
+},  {
+  "hanja": "南",
+  "jei_set": "급08",
+  "mean": "남녘 남"
+}, {
+  "hanja": "女",
+  "jei_set": "급08",
+  "mean": "여자 녀"
+}, {
+  "hanja": "年",
+  "jei_set": "급08",
+  "mean": "해 년"
+}, {
+  "hanja": "大",
+  "jei_set": "급08",
+  "mean": "큰 대"
+}, {
+  "hanja": "東",
+  "jei_set": "급08",
+  "mean": "동녘 동"
+}, {
+  "hanja": "六",
+  "jei_set": "급08",
+  "mean": "여섯 륙"
+}, {
+  "hanja": "母",
+  "jei_set": "급08",
+  "mean": "어미 모"
+}, {
+  "hanja": "木",
+  "jei_set": "급08",
+  "mean": "나무 목"
+}, {
+  "hanja": "門",
+  "jei_set": "급08",
+  "mean": "문 문"
+}, {
+  "hanja": "父",
+  "jei_set": "급08",
+  "mean": "아비 부"
+}, {
+  "hanja": "北",
+  "jei_set": "급08",
+  "mean": "북녘 북, 달아날 배"
+}, {
+  "hanja": "四",
+  "jei_set": "급08",
+  "mean": "넉 사"
+}, {
+  "hanja": "山",
+  "jei_set": "급08",
+  "mean": "메 산"
+}, {
+  "hanja": "三",
+  "jei_set": "급08",
+  "mean": "석 삼"
+}, {
+  "hanja": "生",
+  "jei_set": "급08",
+  "mean": "날 생"
+}, {
+  "hanja": "西",
+  "jei_set": "급08",
+  "mean": "서녘 서"
+}, {
+  "hanja": "先",
+  "jei_set": "급08",
+  "mean": "먼저 선"
+}, {
+  "hanja": "小",
+  "jei_set": "급08",
+  "mean": "작을 소"
+}, {
+  "hanja": "少",
+  "jei_set": "급08",
+  "mean": "적을 소"
+}, {
+  "hanja": "水",
+  "jei_set": "급08",
+  "mean": "물 수"
+}, {
+  "hanja": "室",
+  "jei_set": "급08",
+  "mean": "집 실"
+}, {
+  "hanja": "心",
+  "jei_set": "급08",
+  "mean": "마음 심"
+}, {
+  "hanja": "十",
+  "jei_set": "급08",
+  "mean": "열 십"
+}, {
+  "hanja": "魚",
+  "jei_set": "급08",
+  "mean": "물고기 어"
+}, {
+  "hanja": "五",
+  "jei_set": "급08",
+  "mean": "다섯 오"
+}, {
+  "hanja": "王",
+  "jei_set": "급08",
+  "mean": "임금 왕"
+}, {
+  "hanja": "月",
+  "jei_set": "급08",
+  "mean": "달 월"
+}, {
+  "hanja": "二",
+  "jei_set": "급08",
+  "mean": "두 이"
+}, {
+  "hanja": "人",
+  "jei_set": "급08",
+  "mean": "사람 인"
+}, {
+  "hanja": "一",
+  "jei_set": "급08",
+  "mean": "한 일"
+}, {
+  "hanja": "日",
+  "jei_set": "급08",
+  "mean": "날 일"
+}, {
+  "hanja": "子",
+  "jei_set": "급08",
+  "mean": "아들 자"
+}, {
+  "hanja": "自",
+  "jei_set": "급08",
+  "mean": "스스로 자"
+}, {
+  "hanja": "田",
+  "jei_set": "급08",
+  "mean": "밭 전"
+}, {
+  "hanja": "弟",
+  "jei_set": "급08",
+  "mean": "아우 제"
+}, {
+  "hanja": "第",
+  "jei_set": "급08",
+  "mean": "차례 제"
+}, {
+  "hanja": "中",
+  "jei_set": "급08",
+  "mean": "가운데 중"
+}, {
+  "hanja": "寸",
+  "jei_set": "급08",
+  "mean": "마디 촌"
+}, {
+  "hanja": "七",
+  "jei_set": "급08",
+  "mean": "일곱 칠"
+}, {
+  "hanja": "土",
+  "jei_set": "급08",
+  "mean": "흙 토"
+}, {
+  "hanja": "八",
+  "jei_set": "급08",
+  "mean": "여덟 팔"
+}, {
+  "hanja": "學",
+  "jei_set": "급08",
+  "mean": "배울 학"
+}, {
+  "hanja": "兄",
+  "jei_set": "급08",
+  "mean": "형 형"
+}, {
+  "hanja": "火",
+  "jei_set": "급08",
+  "mean": "불 화"
+}
+];
