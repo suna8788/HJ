89bf6e6f20f3a4c755200da04c8137be751bd6ef
suna8788 <61766463+suna8788@users.noreply.github.com>
2020-03-05 16:11:09 +0900

    Add files via upload

--- card_03.js
+++ card_03.js
@@ -47,22 +47,22 @@ var means1 = ['사람 인', '큰 대', '양 양', '소 우'];
 var hanjas2 = ['女', '鹿', '立', '象', '子', '虎'];
 var means2 = ['여자 녀', '사슴 록', '설 립', '코끼리 상', '아들 자', '호랑이 호'];
 
-cardArea.rect(8, 60, 345, 270).attr({
+cardArea.rect(8, 50, 345, 270).attr({
     'fill': '#c3d8d8',
     'opacity': 0.7
 });
-cardArea.text(43, 140, '가05').attr({
+cardArea.text(43, 130, '가05').attr({
     'font-family': 'Sans-Serif',
     'font-size': 25,
     'fill': '#223C6F',
     'font-weight': 'bold'
 });
 
-cardArea.rect(8, 340, 345, 270).attr({
+cardArea.rect(8, 330, 345, 270).attr({
     'fill': '#c3d8d8',
     'opacity': 0.7
 });
-cardArea.text(43, 420, '가06').attr({
+cardArea.text(43, 410, '가06').attr({
     'font-family': 'Sans-Serif',
     'font-size': 25,
     'fill': '#223C6F',
@@ -70,17 +70,17 @@ cardArea.text(43, 420, '가06').attr({
 });
 
 for (var i = 0; i < 4; i++) {
-    cardArea.rect(130 + (i % 2) * 115, 70 + Math.floor(i / 2) * 280, 100, 120, 3, 3).attr({
+    cardArea.rect(130 + (i % 2) * 115, 60 + Math.floor(i / 2) * 280, 100, 120, 3, 3).attr({
         'fill': 'white',
         'stroke': 'gray'
     });
 
-    cardArea.text(180 + (i % 2) * 115, 130 + Math.floor(i / 2) * 280, hanjas1[i]).attr({
+    cardArea.text(180 + (i % 2) * 115, 120 + Math.floor(i / 2) * 280, hanjas1[i]).attr({
         'font-size': 50,
         'text-anchor': 'middle'
     });
 
-    cardArea.text(180 + (i % 2) * 115, 170 + Math.floor(i / 2) * 280, means1[i]).attr({
+    cardArea.text(180 + (i % 2) * 115, 160 + Math.floor(i / 2) * 280, means1[i]).attr({
         'font-size': 20,
         'text-anchor': 'middle'
     });
@@ -88,28 +88,57 @@ for (var i = 0; i < 4; i++) {
 }
 
 for (var j = 0; j < 6; j++) {
-    cardArea.rect(15 + (j % 3) * 115, 200 + (j % 2) * 280, 100, 120, 3, 3).attr({
+    cardArea.rect(15 + (j % 3) * 115, 190 + (j % 2) * 280, 100, 120, 3, 3).attr({
         'fill': 'white',
         'stroke': 'gray'
     });
 
-    cardArea.text(65 + (j % 3) * 115, 260 + (j % 2) * 280, hanjas2[j]).attr({
+    cardArea.text(65 + (j % 3) * 115, 250 + (j % 2) * 280, hanjas2[j]).attr({
         'font-size': 50,
         'text-anchor': 'middle'
     });
 
-    cardArea.text(65 + (j % 3) * 115, 300 + (j % 2) * 280, means2[j]).attr({
+    cardArea.text(65 + (j % 3) * 115, 290 + (j % 2) * 280, means2[j]).attr({
         'font-size': 20,
         'text-anchor': 'middle'
     });
 
 }
 
-// arrow1.use().transform('t10, 610').click(handler01).appendTo(Paper);
-// function handler01() {
-//     location.replace('bon_03.html');
-// }
-arrow2.use().transform('t330, 610').click(handler02).appendTo(Paper);
+// 하단 버튼
+var buttonArea = Paper.g();
+
+var redB = buttonArea.rect(150, 610, 85, 23).attr({
+  'fill': '#E9967A',
+  'opacity' : 0.8,
+  'rx': 8,
+  'ry': 8
+});
+var redBT = buttonArea.text(167, 626, '깜빡이 카드').attr({
+  'fill': 'black',
+  'font-size': 12
+});
+var red = buttonArea.g(redB, redBT).click(handler01).attr({
+  'cursor': 'pointer'
+});
+
+var orangeB = buttonArea.rect(260, 610, 85, 23).attr({
+  'fill': '#DAA520',
+  'opacity' : 0.8,
+  'rx': 8,
+  'ry': 8
+});
+var orangeBT = buttonArea.text(280, 626, '게임 도전!').attr({
+  'fill': 'black',
+  'font-size': 12
+});
+var orange = buttonArea.g(orangeB, orangeBT).click(handler02).attr({
+  'cursor': 'pointer'
+});
+
+function handler01() {
+  location.replace('flash_03-01.html');
+}
 function handler02() {
-    location.replace('game_03.html');
+  location.replace('game_03.html');
 }
